19aced5b1196aacece194c67282cf37cc4f8a223
Aljoscha Zöller <mail@aljoschazoeller.com>
2026-02-04 19:24:12 +0100

    swap CIDR defaults for public and private subnets

--- variables.tf
+++ variables.tf
@@ -11,11 +11,11 @@ variable "aws_availability_zone_a" {
 variable "public_subnet_1_cidr" {
   description = "CIDR block for Public Subnet 1"
   type = string
-  default = "10.0.1.0/24"
+  default = "10.0.0.0/24"
 }
 
 variable "private_subnet_1_cidr" {
   description = "CIDR block for Private Subnet 1"
   type = string
-  default = "10.0.0.0/24"
+  default = "10.0.1.0/24"
 }
