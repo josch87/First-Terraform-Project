4cd4b4fd7cdb7c76d32f1c8d4820871368a9d1c6
Aljoscha Zöller <mail@aljoschazoeller.com>
2026-02-04 18:41:58 +0100

    add public subnet, igw and public route table

--- main.tf
+++ main.tf
@@ -18,3 +18,36 @@ resource "aws_subnet" "private_subnet_1" {
     Name = "MyPrivateSubnet1"
   }
 }
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id = aws_vpc.my_vpc.id
+  cidr_block = var.public_subnet_1_cidr
+  availability_zone = var.aws_availability_zone_a
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "MyPublicSubnet1"
+  }
+}
+
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "MyInternetGateway"
+  }
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+  tags = {
+    Name = "MyPublicRouteTable"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_association" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
--- variables.tf
+++ variables.tf
@@ -13,3 +13,9 @@ variable "private_subnet_1_cidr" {
   type = string
   default = "10.0.0.0/24"
 }
+
+variable "public_subnet_1_cidr" {
+  description = "CIDR block for Public Subnet 1"
+  type = string
+  default = "10.0.1.0/24"
+}
