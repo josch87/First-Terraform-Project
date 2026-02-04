01668c31df80c501a83cae7fc51e5f00e50c89a6
Aljoscha Zöller <mail@aljoschazoeller.com>
2026-02-04 19:08:48 +0100

    reorder variable and resource definitions for better readability

--- main.tf
+++ main.tf
@@ -1,21 +1,9 @@
-
-
 resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/20"
   enable_dns_support = true
   enable_dns_hostnames = true
   tags = {
     Name = "MyVpc"
-  }
-}
-
-resource "aws_subnet" "private_subnet_1" {
-  vpc_id = aws_vpc.my_vpc.id
-  cidr_block = var.private_subnet_1_cidr
-  availability_zone = var.aws_availability_zone_a
-  map_public_ip_on_launch = false
-  tags = {
-    Name = "MyPrivateSubnet1"
   }
 }
 
@@ -26,6 +14,16 @@ resource "aws_subnet" "public_subnet_1" {
   map_public_ip_on_launch = true
   tags = {
     Name = "MyPublicSubnet1"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id = aws_vpc.my_vpc.id
+  cidr_block = var.private_subnet_1_cidr
+  availability_zone = var.aws_availability_zone_a
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "MyPrivateSubnet1"
   }
 }
 
--- variables.tf
+++ variables.tf
@@ -4,18 +4,18 @@ variable "aws_region" {
 }
 
 variable "aws_availability_zone_a" {
-  description = "First availability zone"
+  description = "Primary availability zone"
   type = string
-}
-
-variable "private_subnet_1_cidr" {
-  description = "CIDR block for Private Subnet 1"
-  type = string
-  default = "10.0.0.0/24"
 }
 
 variable "public_subnet_1_cidr" {
   description = "CIDR block for Public Subnet 1"
   type = string
   default = "10.0.1.0/24"
+}
+
+variable "private_subnet_1_cidr" {
+  description = "CIDR block for Private Subnet 1"
+  type = string
+  default = "10.0.0.0/24"
 }
