173953cc030e8d3b0e1a077b1f9a70c5d2774d5c
Aljoscha Zöller <mail@aljoschazoeller.com>
2026-02-04 14:22:15 +0100

    add vpc and subnet configuration

--- main.tf
+++ main.tf
@@ -1,3 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.17.0"
+    }
+  }
+}
 provider "aws" {
   region = var.aws_region
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/20"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  tags = {
+    Name = "MyVpc"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id = aws_vpc.my_vpc.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = var.aws_availability_zone_a
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "MyPrivateSubnet1"
+  }
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "aws_region" {
   description = "AWS region to deploy into"
   type = string
 }
+
+variable "aws_availability_zone_a" {
+  description = "First availability zone"
+  type = string
+}
